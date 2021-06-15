27385fc7766aef277daa8774a5b0a98a5f65d3cb
Lenovo <Lenovo@DESKTOP-Q1K7C0A>
2021-06-15 12:49:05 +0300

    Java Camp Homework - Day 7 - adding swagger support

--- src/main/java/kodlamaIo/hrms/business/abstracts/JobTitleService.java
+++ src/main/java/kodlamaIo/hrms/business/abstracts/JobTitleService.java
@@ -2,8 +2,12 @@ package kodlamaIo.hrms.business.abstracts;
 
 import java.util.List;
 
+import kodlamaIo.hrms.core.utilities.results.DataResult;
+import kodlamaIo.hrms.core.utilities.results.Result;
 import kodlamaIo.hrms.entities.concretes.JobTitle;
 
 public interface JobTitleService {
-	List<JobTitle> getAll();
+	DataResult<List<JobTitle>> getAll();
+	
+	Result add (JobTitle jobtitles);
 }
--- src/main/java/kodlamaIo/hrms/business/concretes/JobTitleManager.java
+++ src/main/java/kodlamaIo/hrms/business/concretes/JobTitleManager.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaIo.hrms.business.abstracts.JobTitleService;
+import kodlamaIo.hrms.core.utilities.results.DataResult;
+import kodlamaIo.hrms.core.utilities.results.Result;
+import kodlamaIo.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaIo.hrms.core.utilities.results.SuccessResult;
 import kodlamaIo.hrms.dataAccess.abstracts.JobTitleDao;
 import kodlamaIo.hrms.entities.concretes.JobTitle;
 
@@ -21,8 +25,14 @@ public class JobTitleManager implements JobTitleService{
 	}
 
 	@Override
-	public List<JobTitle> getAll() {
-		return this.jobTitleDao.findAll();
+	public DataResult<List<JobTitle>> getAll() {
+		return new SuccessDataResult<List<JobTitle>>(this.jobTitleDao.findAll(), "Data Listelendi");
+	}
+	
+	@Override
+	public Result add(JobTitle titles) {
+		this.jobTitleDao.save(titles);
+		return new SuccessResult("İş Başlığı Eklendi");
 	}
 
 }
